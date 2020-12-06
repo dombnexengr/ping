4383b18c8e03ec0bd1c45817ae7ade6024e7ca4d
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 01:55:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
 
     if (message.content === '!egping') {
 
-       message.channel.send(`🏓 | Latency is: **${Date.now() - message.createdTimestamp}ms.**`);
+       message.channel.send(`:EnigmaLogoBig500: | Latency is: **${Date.now() - message.createdTimestamp}ms.**`);
        }
 
 });
